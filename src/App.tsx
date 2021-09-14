fd7659221fea9fa70885efb071e296ca635408bf
ameenfarooqi <ameenfarooqi25@gmal.com>
2021-09-14 23:03:02 +0530

    commit changes done

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import './App.css';
-import InputInventory from './components/input';
 import Layout from './components/Layout';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 
 function App() {
   return (
-    <Router>
+    
     <div className="App">
+      <Router>
       <Layout />
+      </Router>
       
       
         
     </div>
-    </Router>
   );
 }
 
