7c70856835643180ee57908f47efa775915aeba8
Meer Sawood <msawood@redhat.com>
2021-09-24 01:42:21 +0530

    api url fix 2

--- src/services/APIservice.ts
+++ src/services/APIservice.ts
@@ -5,7 +5,7 @@ type ApiResponse = {
     data: any;
 };
 
-const API_URL = (process.env.REACT_APP_API_SERVER_URL || 'http://inpet-api-server:32000/api')
+const API_URL = (process.env.REACT_APP_API_SERVER_URL || 'http://inpet-api-server-msawood-stage.apps.sandbox-m2.ll9k.p1.openshiftapps.com/api')
 
 async function getSuppliers(){
     const result: ApiResponse = { code: 0, data: {} };
